d9e06bffcc7cd8e41f6cadef877ad2b0aa3fe533
Venky0003 <rajv17270@gmail.com>
2022-11-18 08:47:43 +0530

    complete assignment level 2

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,22 +5,57 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
-
+ let user= prompt("Enter a name");
+   alert(`${user}`);
+ let userName = user;
+ console.log(user, userName);
+ user="John";
+ let age= 22 ,isLearning= true;
+ console.log(user, userName);   
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+let numA =  Number(prompt("Enter a number"));
+let numB =  Number(prompt("Enter a number"));
+let sum = numA+numB;
+alert(`sum is ${sum}`)
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j++) {
+   if(j % 2==0){
+ console.log(j);
+   }
+}  
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
+for (let j = 9; j >=0; j--) {
+    if(j % 2!==0){
+  console.log(j);
+    }
+ }
 
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+let i= 5;
+let concat=""
+while (i >= 0) {
+   concat = concat+i
+   i--;
+}
+console.log(concat)
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+let k= 10;
+let concatEven=""
+while (k >= 0) {
+  if(k % 2 == 0){
+   concatEven = concatEven+k
+  }
+  k--;
+}
+console.log(concatEven)
 
 // Comparisoin
 
@@ -36,3 +71,7 @@ Example:
 "hello", -21; // false
 
 */
+let num1 = prompt("Enter a number one to compare");
+let num2 = prompt("Enter a number two to compare")
+
+num1 ===num2 ? alert(true):alert(false)
